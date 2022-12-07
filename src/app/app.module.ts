8059adb644ac9f0342470fdeaffed1aeb7fce2b6
Benocci <90903554+Benocci@users.noreply.github.com>
2022-12-07 03:56:37 +0100

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TheaterService } from './theater.service';
+import { ShowCreatorComponent } from './show-creator/show-creator.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, ShowCreatorComponent],
   providers: [TheaterService],
   bootstrap:    [ AppComponent ]
 })
--- src/app/show-creator/show-creator.component.ts
+++ src/app/show-creator/show-creator.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { TheaterService } from '../theater.service';
 
 @Component({
   selector: 'app-show-creator',
   templateUrl: './show-creator.component.html',
-  styleUrls: ['./show-creator.component.css']
+  styleUrls: ['./show-creator.component.css'],
 })
 export class ShowCreatorComponent implements OnInit {
+  constructor(private service: TheaterService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  createNewShow() {
+    this.service.newData().subscribe({
+      next: (x: any) => {
+        const teatro = new Array(7)
+          .fill('')
+          .map(() => Array(10).fill('x'))
+          .concat(new Array(4).fill('').map(() => Array(6).fill('x')));
+        this.service.setData(x, teatro).subscribe({
+          next: (x: any) => {
+            alert('Funziona');
+          },
+          error: (err) => {
+            console.error(`Observer got an error: ${JSON.stringify(err)}`);
+          },
+        });
+      },
+      error: (err) => {
+        console.error(`Observer got an error: ${JSON.stringify(err)}`);
+      },
+    });
   }
-
 }
