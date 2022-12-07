0fce02e1c146cdd5155c9d45f478714f992d9e07
Benocci <90903554+Benocci@users.noreply.github.com>
2022-12-07 16:49:44 +0100

    1 file modified, 1 added

--- src/app/open-reservation/open-reservation.component.ts
+++ src/app/open-reservation/open-reservation.component.ts
@@ -2,8 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-openreservation',
-  templateUrl: './openreservation.component.html',
-  styleUrls: ['./openreservation.component.css'],
+  templateUrl: './open-reservation.component.html',
+  styleUrls: ['./open-reservation.component.css'],
 })
 export class OpenreservationComponent implements OnInit {
   @Input() parterre: any[] | undefined;
